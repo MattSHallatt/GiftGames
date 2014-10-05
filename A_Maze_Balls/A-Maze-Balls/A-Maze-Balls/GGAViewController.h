572e1b5f1285fd8aeacca174e2b2e6e0d4efbaaa
Matty180 <matty180_@Matthews-Mac-mini.local>
2014-10-05 21:41:24 +0100

    Created Project Files
    Created a new project from Xcode 5.1.1

---
+++ A_Maze_Balls/A-Maze-Balls/A-Maze-Balls/GGAViewController.h
@@ -0,0 +1,13 @@
+//
+//  GGAViewController.h
+//  A-Maze-Balls
+//
+//  Created by Matthew Hallatt on 05/10/2014.
+//  Copyright (c) 2014 Gift Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGAViewController : UIViewController
+
+@end
---
+++ A_Maze_Balls/A-Maze-Balls/A-Maze-Balls/GGAViewController.m
@@ -0,0 +1,29 @@
+//
+//  GGAViewController.m
+//  A-Maze-Balls
+//
+//  Created by Matthew Hallatt on 05/10/2014.
+//  Copyright (c) 2014 Gift Games. All rights reserved.
+//
+
+#import "GGAViewController.h"
+
+@interface GGAViewController ()
+
+@end
+
+@implementation GGAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
